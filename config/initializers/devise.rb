9665778b803625ced5aa54ef0a0c7b6ef9e8ff53
Ninutsa <ninutsa.nadirashvili@gmail.com>
2024-06-28 15:51:48 +0400

    test confirmation email address

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "no-reply@https://ninutsa-photo-app-cf95b69356c6.herokuapp.com"
+  config.mailer_sender = "no-reply@ninutsa-photo-app-cf95b69356c6.herokuapp.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
